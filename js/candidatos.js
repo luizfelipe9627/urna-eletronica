2e44acc04993fe2ff9e95c414408f21f0b5ae9d7
Luiz Felipe Silva <93567029+luizfelipe9627@users.noreply.github.com>
2022-03-23 00:44:11 -0300

    Update candidatos.js

--- js/candidatos.js
+++ js/candidatos.js
@@ -8,7 +8,7 @@ let etapas = [
                 nome: 'Fulano de Tal',
                 partido: 'ABC',
                 fotos:[
-                    {url:'38111.jpg', legenda: 'Vereador'}
+                    {url:'https://luizfelipe9627.github.io/urna-eletronica/assets/images/38111.jpg', legenda: 'Vereador'}
                 ]
             },
             {
@@ -16,7 +16,7 @@ let etapas = [
                 nome: 'Beltrano da Silva',
                 partido: 'DEFG',
                 fotos:[
-                    {url:'77222.jpg', legenda: 'Vereador'}
+                    {url:'https://luizfelipe9627.github.io/urna-eletronica/assets/images/77222.jpg', legenda: 'Vereador'}
                 ]
             },
         ]
@@ -31,8 +31,8 @@ let etapas = [
                 partido: 'ABC',
                 vice: 'Cic',
                 fotos:[
-                    {url:'99.jpg', legenda: 'Prefeito'},
-                    {url:'99_2.jpg', legenda: 'Vice-Prefeito', small: true}
+                    {url:'https://luizfelipe9627.github.io/urna-eletronica/assets/images/99.jpg', legenda: 'Prefeito'},
+                    {url:'https://luizfelipe9627.github.io/urna-eletronica/assets/images/99_2.jpg', legenda: 'Vice-Prefeito', small: true}
                 ]
             },
             {
@@ -41,8 +41,8 @@ let etapas = [
                 partido: 'QWERTY',
                 vice: 'Zul',
                 fotos:[
-                    {url:'84.jpg', legenda: 'Prefeito'},
-                    {url:'84_2.jpg', legenda: 'Vice-Prefeito', small: true}
+                    {url:'https://luizfelipe9627.github.io/urna-eletronica/assets/images/84.jpg', legenda: 'Prefeito'},
+                    {url:'https://luizfelipe9627.github.io/urna-eletronica/assets/images/84_2.jpg', legenda: 'Vice-Prefeito', small: true}
                 ]
             },
         ]
